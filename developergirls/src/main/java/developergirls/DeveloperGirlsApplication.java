7b6fde6df360c4b3619a360b709880f812723bba
diegoandresantana <diegoandresantana@gmail.com>
2018-03-21 22:34:36 -0400

    d

--- developergirls/src/main/java/developergirls/DeveloperGirlsApplication.java
+++ developergirls/src/main/java/developergirls/DeveloperGirlsApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevelopergirlsApplication {
+public class DeveloperGirlsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevelopergirlsApplication.class, args);
+		SpringApplication.run(DeveloperGirlsApplication.class, args);
 	}
 }
--- developergirls/src/test/java/developergirls/DeveloperGirlsApplicationTests.java
+++ developergirls/src/test/java/developergirls/DeveloperGirlsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DevelopergirlsApplicationTests {
+public class DeveloperGirlsApplicationTests {
 
 	@Test
 	public void contextLoads() {
