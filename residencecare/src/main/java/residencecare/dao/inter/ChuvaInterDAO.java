39a95fdcc3461c63bffca6cfabd1bce69d2ea404
diegoandresantana <diegoandresantana@gmail.com>
2018-04-04 17:37:14 -0400

    dd

--- residencecare/src/main/java/residencecare/dao/inter/ChuvaInterDAO.java
+++ residencecare/src/main/java/residencecare/dao/inter/ChuvaInterDAO.java
@@ -5,12 +5,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import residencecare.entity.Chuva;
- 
-public interface ChuvaInterDAO 
-extends JpaRepository<Chuva,Long >, ChuvaInterCustomDAO {
+
+public interface ChuvaInterDAO extends JpaRepository<Chuva, Long>, ChuvaInterCustomDAO {
 	@Query(value = "select * from Chuva c order by c.id_Chuva desc limit 1", nativeQuery = true)
-    public Chuva lastItem();
-	
-	//;@Query("select c from Customer c where c.email = :email")
-    //Stream<Customer> findByEmailReturnStream(@Param("email") String email);
+	public Chuva lastItem();
+
+	// ;@Query("select c from Customer c where c.email = :email")
+	// Stream<Customer> findByEmailReturnStream(@Param("email") String email);
 }
